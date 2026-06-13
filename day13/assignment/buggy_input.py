f112f6e5efb31c814d24e5d5f6a7635ef7b1fdcd
xietian2024-ui <xietian2024@gmail.com>
2026-06-13 12:27:37 +0800

    Remove day13 folder from GitHub

--- day13/assignment/buggy_input.py
+++
@@ -1,6 +0,0 @@
-while True:
-    try:
-        n = int(input("Number: "))
-        break
-    except ValueError:
-        print("Please type a number.")
--- day13/assignment/sample_error.py
+++
@@ -1,5 +0,0 @@
-# Run: python assignment/sample_error.py
-# Produces a traceback for Assignment D.
-
-n = int("cat")
-print(n)
