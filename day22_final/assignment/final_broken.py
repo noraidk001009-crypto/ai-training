fec0b2c9d2b19086726fa7347afa2cef12cec8a4
xietian2024-ui <xietian2024@gmail.com>
2026-06-23 13:58:41 +0800

    Update day22 only

---
+++ day22_final/assignment/final_broken.py
@@ -0,0 +1,13 @@
+def average(scores):
+    return sum(scores) / len(scores)
+ 
+
+
+
+
+
+
+
+scores = [1, 3]
+sb = average(scores)
+print(sb)
---
+++ day22_final/demos/final_broken.py
@@ -0,0 +1,3 @@
+def average(scores):
+    return sum(scores) / len(scores)
+print(average([]))
