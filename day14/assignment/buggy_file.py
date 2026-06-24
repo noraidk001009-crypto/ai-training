8fc699d5c159fa826255a77656eb5fa810d4451b
xietian2024-ui <xietian2024@gmail.com>
2026-06-24 15:43:46 +0800

    Remove day14, day15 folder from GitHub

--- day14/assignment/buggy_file.py
+++
@@ -1,5 +0,0 @@
-with open("x.txt", "w") as f:
-    f.write("hi\n")
-
-with open("x.txt") as f:
-    print("Content:", f.read().strip())
--- day14/demos/demo_file.py
+++
@@ -1,4 +0,0 @@
-with open("highscore.txt", "w") as f:
-    f.write("9000\n")
-with open("highscore.txt") as f:
-    print("Best:", f.read().strip())
